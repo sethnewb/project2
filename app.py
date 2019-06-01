e29ffb4f10e1b637cc9ba06fd58aa412a57768c5
RyanZ <45775099+ryanzimmerlee@users.noreply.github.com>
2019-06-01 15:03:38 -0400

    updated files for choropleth viz

--- app.py
+++ app.py
@@ -42,11 +42,18 @@ def age(gender):
     clean_dict = dict(query_dict)
     clean_list = [{k:v} for k, v in clean_dict.items()]
 
+    print("test Age", file=sys.stderr)
+
     return jsonify(clean_list)
+
+
 
 #  API route for Medals by Year/Country Route
 @app.route("/medals")
 def medals():
+
+
+    print("test Medals x2", file=sys.stderr)
 
     con = sqlite3.connect('db/olympic_data.db')
 
@@ -54,43 +61,45 @@ def medals():
     SELECT * FROM medals_by_country_by_year
     """
 
+    print("test", file=sys.stderr)
+
     medal_data = pd.read_sql(sql, con)
 
     return medal_data.to_json(orient='records')
     
 #  API route for Connected Scatter Plot 
-@app.route("/buildAll/<build>")
-def buildAll(build):
-    con = sqlite3.connect('db/olympic_data.db')
-    # ?? calling data from table in db, but does {} refer to drop down?
-    sql = f"""
-        SELECT * FROM athlete_build_avg;
-        """
-    athlete_build_avg = pd.read_sql(sql, con)
-    # Somehow I need to define which dropdown menu choice relates to which data choice
-    sel = [
-        athlete_build_avg.Sport,
-        athlete_build_avg.Year,
-        athlete_build_avg.avg_height_M,
-        athlete_build_avg.avg_weight_M,
-        athlete_build_avg.avg_height_F,
-        athlete_build_avg.avg_weight_F
-    ]
+# @app.route("/buildAll/<build>")
+# def buildAll(build):
+#     con = sqlite3.connect('db/olympic_data.db')
+#     # ?? calling data from table in db, but does {} refer to drop down?
+#     sql = f"""
+#         SELECT * FROM athlete_build_avg;
+#         """
+#     athlete_build_avg = pd.read_sql(sql, con)
+#     # Somehow I need to define which dropdown menu choice relates to which data choice
+#     sel = [
+#         athlete_build_avg.Sport,
+#         athlete_build_avg.Year,
+#         athlete_build_avg.avg_height_M,
+#         athlete_build_avg.avg_weight_M,
+#         athlete_build_avg.avg_height_F,
+#         athlete_build_avg.avg_weight_F
+#     ]
     
-    results = db.session.query(*sel).filter(athlete_build_avg.build == build).all()
+#     results = db.session.query(*sel).filter(athlete_build_avg.build == build).all()
     
-    # Create dictionary for infor from dropdown menu
-    athletic_build_data = {}
-    for result in results:
-        athlete_build_avg["Sport"] = result[0]
-        athlete_build_avg["Year"] = result[1]
-        athlete_build_avg["avg_height_M"] = result[2]
-        athlete_build_avg["avg_weight_M"] = result[3]
-        athlete_build_avg["avg_height_F"] = result[4]
-        athlete_build_avg["avg_weight_F"] = result[5]
+#     # Create dictionary for infor from dropdown menu
+#     athletic_build_data = {}
+#     for result in results:
+#         athlete_build_avg["Sport"] = result[0]
+#         athlete_build_avg["Year"] = result[1]
+#         athlete_build_avg["avg_height_M"] = result[2]
+#         athlete_build_avg["avg_weight_M"] = result[3]
+#         athlete_build_avg["avg_height_F"] = result[4]
+#         athlete_build_avg["avg_weight_F"] = result[5]
 
-    print(athletic_build_data)
-    return jsonify(athletic_build_data)
+#     print(athletic_build_data)
+#     return jsonify(athletic_build_data)
 
 
 if __name__ == "__main__":
