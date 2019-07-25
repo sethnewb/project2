396b55f46f69acaf175bd7cbc9916a001a03d0f3
Angie Hilliker <ahillike@richmond.edu>
2019-07-25 17:23:20 -0400

    y axis fix, add line

--- app.py
+++ app.py
@@ -73,67 +73,67 @@ def medals():
 #     return male_waterpolo.to_json(orient='records')
 
 #  API route for Athlete build line graphs (some sports, medium version)
-@app.route("/buildAll/<build>")
-def buildAll(build):
-    con = sqlite3.connect('db/olympic_data.db')
-    
-    # Define which data to pull with each dropdown menu choice
-    columns = ["Sport", "Year"]
-    if build == "femaleHeight":
-        sql = f"""
-        SELECT * FROM female_athlete_mean_df_gymnast;
-        """
-        athlete_build_avg = pd.read_sql(sql, con)
-        columns.append("avg_height")
-
-    elif build == "femaleWeight":
-        sql = f"""
-        SELECT * FROM female_athlete_mean_df_gymnast;
-        """
-        athlete_build_avg = pd.read_sql(sql, con)
-        columns.append("avg_weight")
-    elif build == "maleHeight":
-        sql = f"""
-        SELECT * FROM male_athlete_mean_df_waterpolo;
-        """
-        athlete_build_avg = pd.read_sql(sql, con)
-        columns.append("avg_height")
-    else: #maleWeight
-        sql = f"""
-        SELECT * FROM male_athlete_mean_df_waterpolo;
-        """
-        athlete_build_avg = pd.read_sql(sql, con)
-        columns.append("avg_weight")
-    #Drops to database down to just the three columns in this list, columns
-    athlete_build_avg = athlete_build_avg[columns]
-    # Rename columns to that last column is called "Build" no matter which data is called
-    athlete_build_avg.columns = ["Sport", "Year", "Build"]
-    return athlete_build_avg.to_json(orient = 'records')
-
-
-#  API route for Athlete build line graphs (all sports, complex version)
 # @app.route("/buildAll/<build>")
 # def buildAll(build):
 #     con = sqlite3.connect('db/olympic_data.db')
-#     sql = f"""
-#         SELECT * FROM athlete_build_avg_{build[0]};
-#         """
-#     athlete_build_avg = pd.read_sql(sql, con)
+    
 #     # Define which data to pull with each dropdown menu choice
 #     columns = ["Sport", "Year"]
 #     if build == "femaleHeight":
+#         sql = f"""
+#         SELECT * FROM female_athlete_mean_df_gymnast;
+#         """
+#         athlete_build_avg = pd.read_sql(sql, con)
 #         columns.append("avg_height")
+
 #     elif build == "femaleWeight":
+#         sql = f"""
+#         SELECT * FROM female_athlete_mean_df_gymnast;
+#         """
+#         athlete_build_avg = pd.read_sql(sql, con)
 #         columns.append("avg_weight")
 #     elif build == "maleHeight":
+#         sql = f"""
+#         SELECT * FROM male_athlete_mean_df_waterpolo;
+#         """
+#         athlete_build_avg = pd.read_sql(sql, con)
 #         columns.append("avg_height")
 #     else: #maleWeight
+#         sql = f"""
+#         SELECT * FROM male_athlete_mean_df_waterpolo;
+#         """
+#         athlete_build_avg = pd.read_sql(sql, con)
 #         columns.append("avg_weight")
 #     #Drops to database down to just the three columns in this list, columns
 #     athlete_build_avg = athlete_build_avg[columns]
 #     # Rename columns to that last column is called "Build" no matter which data is called
 #     athlete_build_avg.columns = ["Sport", "Year", "Build"]
 #     return athlete_build_avg.to_json(orient = 'records')
+
+
+#  API route for Athlete build line graphs (all sports, complex version)
+@app.route("/buildAll/<build>")
+def buildAll(build):
+    con = sqlite3.connect('db/olympic_data.db')
+    sql = f"""
+        SELECT * FROM athlete_build_avg_{build[0]};
+        """
+    athlete_build_avg = pd.read_sql(sql, con)
+    # Define which data to pull with each dropdown menu choice
+    columns = ["Sport", "Year"]
+    if build == "femaleHeight":
+        columns.append("avg_height")
+    elif build == "femaleWeight":
+        columns.append("avg_weight")
+    elif build == "maleHeight":
+        columns.append("avg_height")
+    else: #maleWeight
+        columns.append("avg_weight")
+    #Drops to database down to just the three columns in this list, columns
+    athlete_build_avg = athlete_build_avg[columns]
+    # Rename columns to that last column is called "Build" no matter which data is called
+    athlete_build_avg.columns = ["Sport", "Year", "Build"]
+    return athlete_build_avg.to_json(orient = 'records')
 
 
 if __name__ == "__main__":
--- static/connectedScatter.js
+++ static/connectedScatter.js
@@ -6,9 +6,9 @@ function buildConnectedScatter(build) {
   if (!connectedScatterInitialized)
     svg = initSVG(); 
   
-  var margin = {top: 10, right: 30, bottom: 30, left: 60},
-  width = 460 - margin.left - margin.right,
-  height = 400 - margin.top - margin.bottom;
+  var margin = {top: 30, right: 30, bottom: 30, left: 30},
+  width = 925 - margin.left - margin.right,
+  height = 550 - margin.top - margin.bottom;
 
   svg.remove();
   svg = d3.select("#connected_scatter")
@@ -29,89 +29,191 @@ function buildConnectedScatter(build) {
     // set the dimensions and margins of the graph
     console.log("bodyData") 
     console.log(bodyData) 
+  
      // append the svg object to the body of the page
      
-    
-
-    //Read the data
-    
+    //Reformat the json object so that the data is clustered by sport
+    var dataReorg = {};
+    bodyData.forEach(function(d){
+    // if the sport isn't already in the dataReord dictionary
+    if(! (d.Sport in dataReorg))
+      // then add an empty array named after that sport
+      dataReorg[d.Sport] = [];
+      // and push all the data from bodyData for that sport into that array
+      dataReorg[d.Sport].push(d);
+  })
+  // make a list of the keys (sport names) represented in that dictionary
+  for (i=0; i< Object.keys(dataReorg).length; i++);
+  // console.log("dataReorg!!")
+  // console.log(dataReorg)
     
     // Add X axis --> year
-
+    var pad = 30
     var parseTime = d3.timeParse("%Y");
     bodyData.forEach(function(d) {
         d.date = parseTime(d.Year);
-        // console.log(d.date);
-    });
+            });
     var x = d3.scaleTime()
-      .domain([bodyData[0].date, bodyData[bodyData.length-1].date])
-      .range([ 0, width ]);
+    x.domain([bodyData[0].date, bodyData[bodyData.length-1].date]);
+    x.range([200, width - pad]);
+      console.log("bodyData.date");
+      console.log(bodyData[bodyData.length-1].date);
     svg.append("g")
+      .attr("class", "x axis")
       .attr("transform", "translate(0," + height + ")")
       .call(d3.axisBottom(x)
             .tickFormat(d3.timeFormat("%Y")));
+
+  
+
     // Add Y axis
-    var minHeight =100000;
-    var maxHeight = -1;
+    var minBuild =100000;
+    var maxBuild = -1;
     bodyData.forEach(function(d) {
-      if (d.Build > maxHeight) {
-        maxHeight = d.Build;
+      if (d.Build > maxBuild) {
+        maxBuild = d.Build;
       }
-      if (d.Build < minHeight) {
-        minHeight = d.Build;
+      if (d.Build < minBuild) {
+        minBuild = d.Build;
       }
     });
 
+    // Setting y Axis
     var y = d3.scaleLinear()
-      .domain([minHeight-0.1*minHeight, maxHeight+0.1*maxHeight])
+      .domain([minBuild-0.1*minBuild, maxBuild+0.1*maxBuild])
       .range([ height, 0 ]);
-      svg.append("g")
-      .call(d3.axisLeft(y));
-      console.log("minHeight, maxHeight: ");
-      console.log([minHeight, maxHeight]);
+    svg.append("g")
+      .call(d3.axisLeft(y))
+      .attr("class", "axis")
+      .attr("transform", "translate(" + pad + ", 0)")
+      console.log("minBuild, maxBuild: ")
+      console.log([minBuild, maxBuild]);
 
-    // Add the line
+    // Add the data points as path line (take 1, all points are connected as line)
+    var line = d3.line()
+      .x( function(d) { return x(d.date) } )
+      .y( function(d) { return y(d.Build) } )
     svg.append("path")
-      .data(bodyData)
+      .datum(bodyData)
+      .attr("class", "line")
+      .attr("d", line)
       .attr("fill", "none")
       .attr("stroke", "#9eb6c1")
-      .attr("stroke-width", 30)
-      .attr("d", d3.line()
-          .x(function(d) { return x(d.date) })
-          .y(function(d) { return y(d.Build) })
-          );
-      // .on("mouseover", function(d) {   
-      //     d3.select(this).attr("stroke-width", 30)
-      //         .style("stroke", "#63031a");
-      //     div.transition()        
-      //         .duration(70)      
-      //         .style("opacity", .7);      
-      //     div .html(formatTime(d.date) + "<br/>"  + d.close)  
-      //         .style("left", (d3.event.pageX) + "px")     
-      //         .style("top", (d3.event.pageY - 28) + "px");    
-      //     })                  
-      // .on("mouseout", function(d) {       
-      //     d3.select(this).attr("stroke-width", 1.5)
-      //         .style("stroke", "#9eb6c1");
-      //     div.transition()        
-      //         .duration(200)      
-      //         .style("opacity", 0);   
-      // });
-    // Add the points
-    svg
+      .on("mouseover", function(d) {   
+        d3.select(this).attr("r", 5)
+            .style("stroke", "#63031a")  
+        })                  
+    .on("mouseout", function(d) {       
+        d3.select(this).attr("r", 2)
+            .style("stroke", "#9eb6c1")
+        // div.transition()        
+        //     .duration(200)      
+        //     .style("opacity", 0);   
+      });
+
+  // Add the data points as path line (take 2, try to get distinct lines)
+  // var line = d3.svg.line()
+  //     .x( function(d,i) { 
+  //       return x(d.date);
+  //       })
+  //     .y( function(d) { 
+  //       return y(d.Build); 
+  //       });
+  // var sportType = {};
+  // d3.text(***)
+  // svg.append("path")
+  //   .datum(bodyData)
+  //   .attr("class", "line")
+  //   .attr("d", line)
+  //   .attr("fill", "none")
+  //   .attr("stroke", "#9eb6c1")
+  //   .on("mouseover", function(d) {   
+  //     d3.select(this).attr("r", 5)
+  //         .style("stroke", "#63031a")  
+  //     })                  
+  // .on("mouseout", function(d) {       
+  //     d3.select(this).attr("r", 2)
+  //         .style("stroke", "#9eb6c1")
+  //     // div.transition()        
+  //     //     .duration(200)      
+  //     //     .style("opacity", 0);   
+  //   });
+
+// Add the data points as circles
+  svg.append("g")
+    .selectAll("dot")
+    .data(bodyData)
+    .enter()
+    .append("circle")
+        .attr("cx", function(d) { return x(d.date) } )
+        .attr("cy", function(d) { return y(d.Build) } )
+        .attr("r", 2)
+        .attr("fill", "#9eb6c1")
+    // tried mouseover here to see if it works and it does, but it only selects one circle
+    // when the path is working, move this to modify and light up the path
+    .on("mouseover", function(d) {   
+          d3.select(this).attr("r", 5)
+              .style("fill", "#63031a");   
+          })                  
+      .on("mouseout", function(d) {       
+          d3.select(this).attr("r", 2)
+              .style("fill", "#9eb6c1");
+          // div.transition()        
+          //     .duration(200)      
+          //     .style("opacity", 0);   
+      });
+
+      svg
       .append("g")
       .selectAll("dot")
       .data(bodyData)
       .enter()
       .append("circle")
-          .attr("cx", function(d) { return x(d.date) } )
-          .attr("cy", function(d) { return y(d.Build) } )
-          .attr("r", 2)
-          .attr("fill", "#031d5e")
+          
+          .attr("cx", function(d) { if (d.Sport === "gymnastics") {
+            
+          return x(d.date) }} )
+          .attr("cy", function(d) { if (d.Sport === "gymnastics") {
+            return y(d.Build) }} )
+          .attr("r", 5)
+          .attr("fill", "#63031a");
+
+    // Add the path line (still not working)
+      // console.log("dataReorg[d.Sport][i][1]");
+      console.log(dataReorg);
+    
+      svg.append("svg:path")
+        .data(bodyData)
+        .attr("fill", "none")
+        .attr("stroke", "#9eb6c1")
+        .attr("stroke-width", 30)
+        .attr("d", d3.line()
+            .x("x", function(d) { return x(d.date) })
+            .y("y", function(d) { return y(d.Build) })
+            );
+        // eventual code for mouse over effect to darker line
+        // still need to work in "Sport" name as a pop up box label during mouse over
+        // .on("mouseover", function(d) {   
+        //     d3.select(this).attr("stroke-width", 30)
+        //         .style("stroke", "#63031a");
+        //     div.transition()        
+        //         .duration(70)      
+        //         .style("opacity", .7);      
+        //     div .html(formatTime(d.date) + "<br/>"  + d.close)  
+        //         .style("left", (d3.event.pageX) + "px")     
+        //         .style("top", (d3.event.pageY - 28) + "px");    
+        //     })                  
+        // .on("mouseout", function(d) {       
+        //     d3.select(this).attr("stroke-width", 1.5)
+        //         .style("stroke", "#9eb6c1");
+        //     div.transition()        
+        //         .duration(200)      
+        //         .style("opacity", 0);   
+        // });
+
   })
 }
 
-// #031d5e bright blue, #9eb6c1 subtle blue
 // function for when the athletic build drop down menu changes
 function buildChanged(build) {
   buildConnectedScatter(build);
@@ -120,21 +222,17 @@ function buildChanged(build) {
 
 
 // init function is called when the page is loaded. 
-// add necessary functions to it for your graphs
 
 function init() {
-  // build scatter plot with  drop down menu change
-  // will it autopopulate with the first choice automatically or do I need another function for that?
-  
-  
+  // build scatter plot with  drop down menu change  
   var selectedBuild = d3.select("#selBuild").node().value;
   buildConnectedScatter(selectedBuild)
 }
 
 function initSVG() {
-  var margin = {top: 10, right: 30, bottom: 30, left: 60},
-         width = 460 - margin.left - margin.right,
-         height = 400 - margin.top - margin.bottom;
+  var margin = {top: 30, right: 30, bottom: 30, left: 30},
+    width = 925 - margin.left - margin.right,
+    height = 550 - margin.top - margin.bottom;
   var svg = d3.select("#connected_scatter")
          .append("svg")
              .attr("width", width + margin.left + margin.right)
